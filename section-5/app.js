4efe93340ae1e22495acaf49026ce1e375508186
Akash <akash.v.harale@gmail.com>
2025-02-18 10:13:42 +0530

    finished : lecture no. 62 - how middleware works

--- section-5/app.js
+++ section-5/app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("In the next middlware.");
-  next();
+  res.send("<h1>Hello from Express js</h1>");
 });
 //  .use method is used to add a middleware.
 //  next is a function. which pushes a request to the next middlware.
@@ -21,6 +21,13 @@ app.use((req, res, next) => {
 
 // If you will not call a next() function at the bottom the request will not get
 // passed to the next middleware and the browser will be loading.
+
+// when you want to send a response then don't call a next function instead you
+// can send a response. using res.send("<h1>Hello from Express js</h1>")
+
+// .send method is used to send a response.
+// after sending a response the request will end there only and if there are any other
+// middlwares after that then the request will not reach there.
 
 const server = http.createServer(app);
 
