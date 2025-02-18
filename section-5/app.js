db33a59537ae56b4cc695f96ed4cccbe1d82e955
Akash <akash.v.harale@gmail.com>
2025-02-18 11:39:09 +0530

    finished : 65 - parsing incoming request

--- section-5/app.js
+++ section-5/app.js
@@ -1,39 +1,24 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the first middlware.");
-  next();
+app.use(bodyParser.urlencoded({ extended: false })); //this code is written
+//  first becase we need the request's body before it goes to any routes.
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/products" method="POST"><input type="text" name="title" ><button type="submit">send</button></input></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In the next middlware.");
-  res.send("<h1>Hello from Express js</h1>");
+app.use("/products", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
-//  .use method is used to add a middleware.
-//  next is a function. which pushes a request to the next middlware.
 
-//  when you call a next function at the bottom then it allows you to
-// enter into a new middleware.
-
-// If you will not call a next() function at the bottom the request will not get
-// passed to the next middleware and the browser will be loading.
-
-// when you want to send a response then don't call a next function instead you
-// can send a response. using res.send("<h1>Hello from Express js</h1>")
-
-// .send method is used to send a response.
-// after sending a response the request will end there only and if there are any other
-// middlwares after that then the request will not reach there.
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express .js</h1>");
+});
 
 app.listen(3000);
-
-//we have removed all code of creating a server because app.listen create a
-// server internally by running the same code which we were runnning. i.e
-
-// const server=http.createServer(app);
-// server.listen(3000);
-
