9384754818fead661cc25082e396bb913d0e173f
Akash <akash.v.harale@gmail.com>
2025-02-18 13:50:13 +0530

    finished : lecture no. 69 - filtering path

--- section-5/routes/admin.js
+++ section-5/routes/admin.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+// # get request --> /admin/add-product
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/products" method="POST"><input type="text" name="title" ><button type="submit">send</button></input></form>'
+    '<form action="/admin/products" method="POST"><input type="text" name="title" ><button type="submit">send</button></input></form>'
   );
 });
 
+// # post request --> /admin/add-product
 router.post("/products", (req, res, next) => {
   console.log(req.body);
   res.send("<p>item added</p>");
