228919d7c554873ff29c3dcb74e8d50d04b02339
Akash <akash.v.harale@gmail.com>
2025-03-21 13:23:21 +0530

    feat : nodemon for autostart server

--- section-4/app.js
+++ section-4/app.js
@@ -1,6 +1,9 @@
 const http = require("http");
-const routes = require("./routes");
 
-const server = http.createServer(routes);
+const server = http.createServer((req, res) => {
+  res.write(
+    "<html><head><title>My first page</title></head><input type='text' placeholder='name' /><body></body></html>"
+  );
+});
 
 server.listen(3000);
