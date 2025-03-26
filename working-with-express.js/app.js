2eef46a03300a21b905dcfd7f7df458d473e8a28
Akash <akash.v.harale@gmail.com>
2025-03-26 21:32:25 +0530

    added middlwares

--- working-with-express.js/app.js
+++ working-with-express.js/app.js
@@ -1,6 +1,18 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  console.log("server is running on port 3000");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In this first middleware");
+  next(); // Allows the request to proceed to the next middleware function
 });
+
+app.use((req, res, next) => {
+  console.log("In this second middleware");
+  res.send("<h1>Hello from Express!</h1>"); // If you don't call next(), the request will be stopped here and you have to provide a response.
+});
+
+const server = http.createServer(app);
 server.listen(3000);
