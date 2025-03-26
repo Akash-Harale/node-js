c1dd78da732327fd5a37879a450154a112f86cc6
Akash <akash.v.harale@gmail.com>
2025-03-27 00:40:06 +0530

    parsing incoming request and handling different routes

--- working-with-express.js/app.js
+++ working-with-express.js/app.js
@@ -1,25 +1,22 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In this first middleware");
-  next(); // Allows the request to proceed to the next middleware function
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form method='post' action='/products'><input type='text' name='product'><button type='submit'>Add Product</button></form>"
+  );
 });
-
-app.use((req, res, next) => {
-  console.log("In this second middleware");
-  res.send("<h1>Hello from Express!</h1>"); 
+app.use("/products", (req, res) => {
+  console.log(`request body:`, req.body);
+  res.redirect("/");
 });
-
-
-// const server = http.createServer(app);
-// server.listen(3000);
-
-//  OR
+app.use("/", (req, res, next) => {
+  res.send("<h1> This is a default page</h1>");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-}); 
+});
